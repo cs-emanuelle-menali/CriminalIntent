f810faad5e7010e12d440d9abb83c8765ad15401
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-10-25 14:42:37 -0200

    Criar método para reportar crime
    Esse método foi criado para que seja compartilhado o crime informado pelo usuário através das opções de compartilhamento do Android.

--- app/src/main/java/com/example/emanuellemenali/criminalintent/view/fragment/CrimeFragment.java
+++ app/src/main/java/com/example/emanuellemenali/criminalintent/view/fragment/CrimeFragment.java
@@ -48,6 +48,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mCrimeDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mReportButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -119,6 +120,18 @@ public class CrimeFragment extends Fragment {
 
                 mCrime.setmSolved(isChecked);
 
+            }
+        });
+
+        mReportButton = (Button) view.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT,
+                        getString(R.string.crime_report_subject));
+                startActivity(i);
             }
         });
 
